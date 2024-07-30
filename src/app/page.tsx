df7b70059f76bc777d914a97998631c1ea15da61
SagarNRao <sagarnrao154@gmail.com>
2024-07-30 17:48:26 +0530

    adde basic functions - connect button and main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,42 +16,7 @@ import {
 import Web3ModalProvider from "./context";
 import { useState, useEffect } from "react";
 import { QueryClient } from "@tanstack/react-query";
-import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5/react'
-
-// 1. Get a Project ID from https://cloud.walletconnect.com/ 
-const projectId = 'e716a69c7e2e023e9484c136f6a1a079'
-
-// 2. Set chains
-const celo = {
-  chainId: 42220,
-  name: 'Celo',
-  currency: 'CELO',
-  explorerUrl: 'https://explorer.celo.org/mainnet',
-  rpcUrl: 'https://forno.celo.org'
-}
-
-const alfajores = {
-  chainId: 44787,
-  name: 'Alfajores',
-  currency: 'CELO',
-  explorerUrl: 'https://explorer.celo.org/alfajores',
-  rpcUrl: 'https://alfajores-forno.celo-testnet.org'
-}
-
-// 3. Create modal
-const metadata = {
-  name: 'My Celo App',
-  description: 'My Website description',
-  url: 'https://mywebsite.com',
-  icons: ['https://avatars.mywebsite.com/']
-}
-
-createWeb3Modal({
-  ethersConfig: defaultConfig({ metadata }),
-  chains: [celo, alfajores],
-  defaultChain: celo,
-  projectId
-})
+import { createWeb3Modal, defaultConfig } from "@web3modal/ethers5/react";
 
 
 const client = new QueryClient();
@@ -63,7 +28,17 @@ export default function Home() {
   }, []);
   return (
     <>
-      <w3m-button />
+      {/* <w3m-button /> */}
+      <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <div style={{ width: "1200px" }}></div>
+            </NavigationMenuItem>
+            <NavigationMenuItem className="flex">
+              <w3m-button />
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       <main className="flex justify-center items-center h-screen">
         <AddProject />
       </main>
